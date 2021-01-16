8e7f3977cdb05d1167f3cc3916b83cad53e2f63f
Bookis Worthy <bookis.worthy@gmail.com>
2021-01-15 21:52:40 -0800

    Add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '216ccbb1bf38ebe44561a9367d427d8fa93a4333',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '216ccbb1bf38ebe44561a9367d427d8fa93a4333',
+    ),
+  ),
+);
